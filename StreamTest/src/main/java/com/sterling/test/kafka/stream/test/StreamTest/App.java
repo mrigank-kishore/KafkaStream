7fc3084c03c260f8b0f4664647ed314dfde336b6
Mrigank.Varma@kevinladdsterlingbackcheck.onmicrosoft.com <Mrigank.Varma@kevinladdsterlingbackcheck.onmicrosoft.com>
2019-11-14 18:36:16 +0530

    initial commit

---
+++ StreamTest/src/main/java/com/sterling/test/kafka/stream/test/StreamTest/App.java
@@ -0,0 +1,41 @@
+package com.sterling.test.kafka.stream.test.StreamTest;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-starter-app");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> kStream = builder.stream("test-first-topic");
+
+        KStream<String, String> optopic = kStream.mapValues((key, value) -> value.toUpperCase());
+        
+        optopic.to("testoutput");
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.cleanUp(); // only do this in dev - not in prod
+        streams.start();
+
+        // print the topology
+        streams.localThreadsMetadata().forEach(data -> System.out.println(data));
+
+        // shutdown hook to correctly close the streams application
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+}
